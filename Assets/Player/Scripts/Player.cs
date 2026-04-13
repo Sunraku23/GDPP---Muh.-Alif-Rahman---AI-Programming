5e4847d9f91248e4a2d88cd7e44b6d82a374900c
Sunraku23 <sunraku493@gmail.com>
2026-04-13 09:46:58 +0700

    Physics
    Add Physics movement to the player

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        Debug.Log("Horizontal" + horizontal);
+        Debug.Log("Vertical" +  Vertical);
     }
 }
