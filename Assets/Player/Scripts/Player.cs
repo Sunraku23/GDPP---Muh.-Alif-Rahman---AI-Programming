e195df98c645d57a71c74f981a25a30496cbda29
Sunraku23 <sunraku493@gmail.com>
2026-04-14 11:03:36 +0700

    Add Cinemachine

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody rb;
+
+    //Method Awake ini akan jalan ketika game start   
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+
         float horizontal = Input.GetAxis("Horizontal");
         float Vertical = Input.GetAxis("Vertical");
 
         Vector3 movementDirection = new Vector3(horizontal, 0, Vertical);
 
+        rb.linearVelocity = movementDirection * speed * Time.fixedDeltaTime; 
         Debug.Log("Horizontal" + horizontal);
         Debug.Log("Vertical" +  Vertical);
     }
