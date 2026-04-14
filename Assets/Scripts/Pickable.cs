6d2e3aa9b08c38664979ca842ee8431ba7f49cac
Sunraku23 <sunraku493@gmail.com>
2026-04-14 22:43:12 +0700

    Add feature
    Menambahkan pickable manager dan mengubah coin dan power up menjadi pre fab

--- Assets/Scripts/Pickable.cs
+++ Assets/Scripts/Pickable.cs
@@ -1,14 +1,17 @@
+using System;
 using UnityEngine;
 
 public class Pickable : MonoBehaviour
 {
     [SerializeField]
     public PickableType PickableType;
+    public Action<Pickable> OnPicked;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("TWinggg" + PickableType);
+            OnPicked(this);
             Destroy(gameObject);
         }
         
---
+++ Assets/Scripts/PickableManager.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PickableManager : MonoBehaviour
+{
+    private List<Pickable> _pickablelist = new  List<Pickable>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InitPickableList();         
+    }
+
+    private void InitPickableList()
+    {
+        Pickable[] pickableObjects = GameObject.FindObjectsOfType<Pickable>();
+        for(int i = 0; i < pickableObjects.Length; i++)
+        {
+            _pickablelist.Add(pickableObjects[i]);
+            pickableObjects[i].OnPicked += OnPickablePicked; 
+        }
+        Debug.Log("pickable List: " + _pickablelist.Count);
+    }
+
+    private void OnPickablePicked(Pickable pickable)
+    {
+        _pickablelist.Remove(pickable);
+        Debug.Log("pickable List: " + _pickablelist.Count);
+        if (_pickablelist.Count <= 0)
+        {
+            Debug.Log("Win");
+        }
+    }
+
+}
