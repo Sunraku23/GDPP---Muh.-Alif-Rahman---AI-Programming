fbd8b8c9cae5d75003d5075b052a6ecd3b88fd9d
Sunraku23 <sunraku493@gmail.com>
2026-04-15 18:36:40 +0700

    Mengatur Patrol
    Mengatur Arah enemy Patrol

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    public List<Transform> Waypoints = new List<Transform>();
+
     private BaseState currentState;
 
     [HideInInspector]
@@ -10,12 +15,15 @@ public class Enemy : MonoBehaviour
     public ChaseState ChaseState = new ChaseState();
     [HideInInspector]
     public RetreatState RetreatState = new RetreatState();
+    [HideInInspector]
+    public NavMeshAgent NavMeshAgent;
 
 
     private void Awake()
     {
         currentState = PatrolState;
-        currentState.EnterState(this);    
+        currentState.EnterState(this);  
+        NavMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
